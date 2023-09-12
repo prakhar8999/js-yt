a84dff48025dc010ec71bb501e6a6102319e94a2
prakhar8999 <33159783+prakhar8999@users.noreply.github.com>
2023-09-12 20:12:03 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version
+
+let name = "Prak"
+let age = 18
+let isLoggedIn = true
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned yet
+//symbol => unique( mostly will be talked in react lecture)
+
+console.log(typeof isLoggedIn)
+console.log(typeof undefined)
+console.log(typeof null)
