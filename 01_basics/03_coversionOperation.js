38920c4be3c3830bc042a4d5d1e1bdfd2cdafd01
prakhar8999 <33159783+prakhar8999@users.noreply.github.com>
2023-09-12 20:12:28 +0000

    conversion

---
+++ 01_basics/03_coversionOperation.js
@@ -0,0 +1,21 @@
+let score = "8999abc";
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = 14563
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+//1 => true ; 2 => false
+//"" => false
+//"prakhar" => true
