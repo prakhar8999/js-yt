34deca77004f9d2e6a2bc48a1bda6e3e4d3ba154
prakhar8999 <33159783+prakhar8999@users.noreply.github.com>
2023-09-14 20:29:52 +0000

    Comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+//console.log(2<5); //return Boolean
+//console.log(1 == 11);
+
+console.log("2" > 1);
+console.log("prak" > 22);
+console.log(null == 0);
+console.log(null > -1)
+//the reason is that am equality check ( == ) and  comparison ( > < >= <= ) works differently
+//Comparison convert null to number treating it as 0
+//equality check and comparison works same for undefined it always give false
+//=== it checks datatypes also
+
+console.log("2" == 2);
+console.log("2" === 2);
