cba16fb7c0d93f0f21e046080eba8b579adc0e90
prakhar8999 <33159783+prakhar8999@users.noreply.github.com>
2023-09-17 21:53:18 +0000

    string_in_detail

---
+++ 01_basics/06_string.js
@@ -0,0 +1,18 @@
+const name = "prak"
+const count = 100
+
+console.log(name + count + " Value");
+
+//Now-a-days below syntax is used which is known as backticks
+console.log(`Hello my name is ${name} and count is ${count}`)
+
+const gameName = new String('Gts Vice City')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName);
+console.log(gameName.charAt(15));
+
+//there are many functions for string, we can read about that in MDN Doccumentation
